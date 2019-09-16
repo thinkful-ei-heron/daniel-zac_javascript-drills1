c7dbdac10c8b10c074f215fa31cfc4ff60e3d0fa
Daniel Bright <brahyt@gmail.com>
2019-09-16 11:57:12 -0700

    infinity stuff

--- drills2.js
+++ drills2.js
@@ -2,4 +2,19 @@ function jediName(firstName, lastName) {
     return `${lastName.slice(0,3)}${firstName.slice(0,2)}`;
 }
 const myJedi = jediName('Zacharia', 'Lutz');
-console.log(myJedi);
+//console.log(myJedi);
+
+function beyond(num) {
+  if (num === Infinity) {
+    console.log('And beyond');
+  }
+  else if (num > 0) {
+    console.log('To infinity');
+  }
+  else if (num < 0) {
+    console.log('To negative inifity');
+  }
+  else {
+    console.log('Staying home');
+  }
+}
