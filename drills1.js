7fbeeac9abfde7c55cfbde81e29622c51fa48ab4
Zacharia Lutz <zacharia.lutz@gmail.coom>
2019-09-16 13:35:11 -0500

    Added input error

--- drills1.js
+++ drills1.js
@@ -7,16 +7,19 @@ function getYearOfBirth(age) {
   return 2019 - age;
 }
 
+function createGreeting(name, age) {
+  if (name === undefined || age === undefined) {
+    throw new Error('Arguments not valid');
+  }
+  const yearOfBirth = getYearOfBirth(age);
+  return `Hi, my name is ${name} and I'm ${age} years old. I was born in ${yearOfBirth}.`;
+}
+
 try {
   const greeting1 = createGreeting('Zac', 30);
   console.log(greeting1);
 } catch(e) {
   console.error(e);
-}
-
-function createGreeting(name, age) {
-  const yearOfBirth = getYearOfBirth(age);
-  return `Hi, my name is ${name} and I'm ${age} years old. I was born in ${yearOfBirth}.`;
 }
 
 
