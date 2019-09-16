f5c8d388d9b7151e988dcdb8165a8dd5a93b0aff
Daniel Bright <brahyt@gmail.com>
2019-09-16 11:30:06 -0700

    created try/catch

--- drills1.js
+++ drills1.js
@@ -1,14 +1,22 @@
 'use strict'
 
 function getYearOfBirth(age) {
+  if (age < 0) {
+    throw new Error('Age cannot be negative');
+  }
   return 2019 - age;
+}
+
+try {
+  const greeting1 = createGreeting('Zac', 30);
+  console.log(greeting1);
+} catch(e) {
+  console.error(e);
 }
 
 function createGreeting(name, age) {
   const yearOfBirth = getYearOfBirth(age);
-  return `Hi, my name is ${name} and I\'m ${age} years old. I was born in ${yearOfBirth}.`;
+  return `Hi, my name is ${name} and I'm ${age} years old. I was born in ${yearOfBirth}.`;
 }
 
-const greeting1 = createGreeting('Zac', 30);
-console.log(greeting1);
 
