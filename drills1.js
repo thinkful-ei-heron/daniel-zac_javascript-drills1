a3d9c0ccff2919189044fa977586ca1ad38df083
Zacharia Lutz <zacharia.lutz@gmail.coom>
2019-09-16 13:13:39 -0500

    Seperated into two functions

--- drills1.js
+++ drills1.js
@@ -1,8 +1,12 @@
 'use strict'
 
+function getYearOfBirth(age) {
+  return 2019 - age;
+}
+
 function createGreeting(name, age) {
-  const yearOfBirth = 2019 - age;
-  return `Hi, my name is ${name} and I\'m ${age} years old. I was born on ${yearOfBirth}`;
+  const yearOfBirth = getYearOfBirth(age);
+  return `Hi, my name is ${name} and I\'m ${age} years old. I was born in ${yearOfBirth}.`;
 }
 
 const greeting1 = createGreeting('Zac', 30);
