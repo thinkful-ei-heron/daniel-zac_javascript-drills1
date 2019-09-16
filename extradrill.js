5cdba7890beb7237a4f36c5e12b92d4d265ef66c
Daniel Bright <brahyt@gmail.com>
2019-09-16 15:56:37 -0700

    cleaned up code

--- extradrill.js
+++ extradrill.js
@@ -1,3 +1,5 @@
+'use strict';
+
 function decode(string){
   let result = '';
   let arr = string.split(' ');
@@ -6,17 +8,18 @@ function decode(string){
     b: 2,
     c: 3,
     d: 4
-  }
+  };
+
   for (let i = 0; i < arr.length; i++){
     let currentChar = arr[i][0];
     let currentIndex = cypher[`${currentChar}`];
     if(arr[i][currentIndex] !== undefined) {
-     result += arr[i][currentIndex]
+      result += arr[i][currentIndex];
     }
     else {
-      result += " ";
+      result += ' ';
     }
   }
- return result
+  return result;
 }
-console.log(decode('craft block argon meter bells brown croon droop'))
+console.log(decode('craft block argon meter bells brown croon droop'));
