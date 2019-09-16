a10279a783f61554f6cb203c4e01c53f4c285340
Zacharia Lutz <zacharia.lutz@gmail.coom>
2019-09-16 14:08:35 -0500

    mid-work on decoder

--- drills2.js
+++ drills2.js
@@ -1,3 +1,6 @@
+'use strict'
+
+// eslint-disable-next-line strict
 function jediName(firstName, lastName) {
     return `${lastName.slice(0,3)}${firstName.slice(0,2)}`;
 }
@@ -18,3 +21,22 @@ function beyond(num) {
     console.log('Staying home');
   }
 }
+
+function decode(word) {
+  let result = '';
+  let arr = word.split(' ');
+  let code = {
+    a: 1,
+    b: 2,
+    c: 3,
+    d: 4,
+  };
+  
+  for (let i = 0; i < arr.length; i++) {
+    arr[i][0]
+    result += ''
+  }
+}
+
+const message = decode('craft block argon meter bells brown croon droop');
+console.log(message);
