3d43adf1e5fab3f575bb07a58e709d13c32a1576
Daniel Bright <brahyt@gmail.com>
2019-09-16 14:32:04 -0700

    added object keys

---
+++ extradrill.js
@@ -0,0 +1,22 @@
+function decode(string){
+  let result = '';
+  let arr = string.split(' ');
+  let cypher = {
+    a: 1,
+    b: 2,
+    c: 3,
+    d: 4
+  }
+  for (let i = 0; i < arr.length; i++){
+    let currentChar = arr[i][0];
+    let currentIndex = cypher[`${currentChar}`];
+    if(arr[i][currentIndex] !== undefined) {
+     result += arr[i][currentIndex]
+    }
+    else {
+      result += " ";
+    }
+  }
+ return result
+}
+console.log(decode('craft block argon meter bells brown croon droop'))
