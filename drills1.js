6e90915b93bcdac472e8e83e625d62f736cd7ba9
Daniel Bright <brahyt@gmail.com>
2019-09-16 11:42:51 -0700

    added datatype check to name

--- drills1.js
+++ drills1.js
@@ -1,19 +1,21 @@
 'use strict'
 
 function getYearOfBirth(age) {
-  if (age < 0) {
-    throw new Error('Age cannot be negative');
-  }
-
-  if (typeof age !== 'number') {
-    throw new Error('Age must be a number');
-  }
   return 2019 - age;
 }
 
 function createGreeting(name, age) {
   if (name === undefined || age === undefined) {
     throw new Error('Arguments not valid');
+  }
+  if (age < 0) {
+    throw new Error('Age cannot be negative');
+  }
+  if (typeof age !== 'number') {
+    throw new Error('Age must be a number');
+  }
+  if (typeof name !== 'string') {
+    throw new Error('Name must be a string');
   }
   const yearOfBirth = getYearOfBirth(age);
   return `Hi, my name is ${name} and I'm ${age} years old. I was born in ${yearOfBirth}.`;
